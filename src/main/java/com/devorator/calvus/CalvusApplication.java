1480e8419c8d4ab11c1af4529fe459e79bfbb296
Gruszka <jkkrutak@gmail.com>
2021-05-01 18:10:10 +0200

    Setup

---
+++ src/main/java/com/devorator/calvus/CalvusApplication.java
@@ -0,0 +1,13 @@
+package com.devorator.calvus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalvusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalvusApplication.class, args);
+	}
+
+}
