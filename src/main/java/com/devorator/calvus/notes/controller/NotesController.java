63edccd261a3fba578379eaaed5dc5eef716d97f
Gruszka <jkkrutak@gmail.com>
2021-05-01 21:35:13 +0200

    Working connection with database on localhost

--- src/main/java/com/devorator/calvus/notes/controller/NotesController.java
+++ src/main/java/com/devorator/calvus/notes/controller/NotesController.java
@@ -17,7 +17,8 @@ public class NotesController {
     }
 
     public void insertNote(){
-        String sql = "INSERT INTO notes (note_ID, note_NAME, creation_DATE, modified, content) VALUES (1, 'Test', '2017-06-15', '2017-06-15', ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. In malesuada libero non egestas lobortis. Maecenas aliquet enim eu auctor pulvinar. Vestibulum faucibus facilisis augue, quis finibus mi sagittis vel. Cras nisl tellus, laoreet placerat feugiat sed, malesuada commodo mi. Fusce vitae auctor elit, id congue massa. Cras ac placerat turpis. Morbi congue nulla at varius efficitur. Pellentesque ullamcorper leo eget vulputate laoreet.'";
+        String sql = "INSERT INTO notes (note_NAME, creation_DATE, modified, content) VALUES ('Test', '2017-06-05', '2018-02-02', '')";
+
         int result = jdbcTemplate.update(sql);
 
         if (result > 0) {
